f820cf3659e8b5e154cc851efd3be36cc9888fdf
Émilie Desmarais <emilied2002@gmail.com>
2024-10-29 15:01:08 -0400

    s2c1 debut structure de theme

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * index.php - Le modèle par défaut de wordpress
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>31W</title>
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <header class="entete">
+    <section class="global">
+      <h1>31W</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">À propos</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+      <form class="recherche">
+        <input type="search" name="" id="" />
+        <img
+          src="https://s2.svgbox.net/hero-outline.svg?ic=search&color=000"
+          width="20"
+          height="20" />
+      </form>
+    </section>
+  </header>
+  <main class="principal">
+    <section class="global">
+      <h2>Accueil</h2>
+        <?php if (have_posts()): ?>
+          <?php while(have_posts()) : the_post() ?>
+            <?php
+              $chaine = get_the_title();
+              $sigle = substr($chaine,0, 7);
+              $titre = substr($chaine,8, 60);
+            ?>
+            <article class="principal__article">
+              <h5><?php echo $titre; ?></h5>
+              <h6><?php echo $sigle; ?></h6>
+              <p><?php echo wp_trim_words(get_the_excerpt(), 20, null); ?></p>
+            </article>
+          
+          <?php endwhile; ?>
+        <?php endif; ?>
+
+    </section>
+  </main>
+  <footer class="pied">
+    <section class="global">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+    </section>
+  </footer>
+  <?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Projet de session du cours 31W
+
+## Introduction à un système de gestion de contenu
+
+### Les objectifs du thème
+
+- Vous aider à réaliser votre propre thème
+- Documenter les concepts de base
+- Ajouter les références qui ont permis de solutionner certains problèmes
+
+## Exercice-2
+
+### objectif : Réaliser un thème de base
+
+- Améliorer la structure Sass
+- Créer une page d'accueil avec front-page.php
+
+- Auteur : Emilie Desmarais
+- Adresse URL du site sur WHC : https://gftnth00.mywhc.ca/31w03/
